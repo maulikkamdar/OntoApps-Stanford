bdd0519f7ffc1dac81f13f93a702f4583dfed758
maulikkamdar <maulik@maulik-kamdar.com>
2018-07-12 10:29:59 -0700

    Updated version

--- app.py
+++ app.py
@@ -5,29 +5,28 @@ import re, math, rdflib
 import logging
 from SPARQLWrapper import SPARQLWrapper, JSON
 
-#from ontoreuseapp.ontoreuse import ontoreuse
-#from visionapp.vision import vision
-#from bionicapp.bionic import bionic
-#from phlegraapp.phlegra import phlegra
-#from m3app.m3crnn import m3crnn
-#from presenceapp.presence import presence
-#from prismapp.prism import prism
-#from genomesnipapp.genomesnip import genomesnip
+from ontoreuseapp.ontoreuse import ontoreuse
+from visionapp.vision import vision
+from bionicapp.bionic import bionic
+from phlegraapp.phlegra import phlegra
+from m3app.m3crnn import m3crnn
+from presenceapp.presence import presence
+from prismapp.prism import prism
+from genomesnipapp.genomesnip import genomesnip
 from reveald2app.reveald2 import reveald2
 from lslodminerapp.lslodminer import lslodminer
-#from revealdapp.reveald import reveald
-# @TODO divide this file into separate applications using FLASK blueprints
+from revealdapp.reveald import reveald
 
 app = Flask(__name__)
-#app.register_blueprint(ontoreuse)
-#app.register_blueprint(vision)
-#app.register_blueprint(bionic)
-#app.register_blueprint(m3crnn)
-#app.register_blueprint(phlegra)
-#app.register_blueprint(presence)
-#app.register_blueprint(prism)
-#app.register_blueprint(genomesnip)
-#app.register_blueprint(reveald)
+app.register_blueprint(ontoreuse)
+app.register_blueprint(vision)
+app.register_blueprint(bionic)
+app.register_blueprint(m3crnn)
+app.register_blueprint(phlegra)
+app.register_blueprint(presence)
+app.register_blueprint(prism)
+app.register_blueprint(genomesnip)
+app.register_blueprint(reveald)
 app.register_blueprint(reveald2)
 app.register_blueprint(lslodminer)
 
